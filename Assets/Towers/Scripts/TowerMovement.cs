dbc416bd027b2aff085d1197ab43acbb370530dc
ehernandez53 <datptsdb@gmail.com>
2020-09-21 17:59:00 -0700

    5th

---
+++ Assets/Towers/Scripts/TowerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerMovement : MonoBehaviour
+{
+
+    public Transform target;
+    public float range = 2f;
+
+    public string enemyTag = "Enemy";
+    public Transform partToRotate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Checks for targets in range
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        // Finds GameObjects that have been tagged as enemies
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        // If the closest enemy is in range, that enemy becomes the target, else no target
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+            return;
+
+        /* For tower rotation; Not yet sure if necessary
+         
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = lookRotation.eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+        
+         */
+    }
+}
