3b09476a271ce5cae85eb38da54f2061374fd24f
calvin ngo <calvin_ngo@csu.fullerton.edu>
2020-09-26 02:21:27 -0700

    added button

---
+++ Assets/Scenes/TowerPlacementButtonTEst/Test Script for Button/PlaceTower1Test.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using System;
+ 
+public class PlaceTower1Test : MonoBehaviour {
+
+    public static PlaceTower1Test buildTower1;
+
+    void Awake()
+    {
+        if(buildTower1 != null)
+        {
+            return;
+        }
+        buildTower1 = this;
+    }
+
+    private Tilemap tilemap;
+    public Tile normalTile;
+    public Tile highlightTile;
+    public Tile pathTile;
+ 
+    //private Ray ray;
+    private Vector3Int newMousePos;
+    private Vector3Int oldMousePos;
+
+    bool[] validTiles;
+
+    public GameObject tower;
+    private TowerMovement towerScript;
+
+    private void OnMouseOver() {
+ 
+        if (oldMousePos != newMousePos) {
+            if (tilemap.GetTile<Tile>(oldMousePos) == highlightTile) {
+                tilemap.SetTile(oldMousePos, normalTile);
+            }
+            oldMousePos = newMousePos;
+        }
+ 
+        if (tilemap.GetTile<Tile>(newMousePos) == normalTile && tilemap.HasTile(newMousePos)) {
+            tilemap.SetTile(newMousePos, highlightTile);
+        }
+    }
+
+     private void OnMouseDown() {
+
+        int relativeX = newMousePos[0] - tilemap.cellBounds.xMin;
+        int relativeY = newMousePos[1] - tilemap.cellBounds.yMin;
+        int tileIndex = relativeX + (tilemap.cellBounds.size[0] * relativeY);
+        
+        if (validTiles[tileIndex]) {
+            Instantiate(tower, new Vector3Int(relativeX, relativeY, 0) , Quaternion.identity);
+            validTiles[tileIndex] = false;
+        }
+    }
+    
+    private void Start() {
+        tilemap = gameObject.GetComponent<Tilemap>();
+        oldMousePos = new Vector3Int(0, 0, 0);
+
+        BoundsInt bounds = tilemap.cellBounds;
+        TileBase[] tileArray = tilemap.GetTilesBlock(bounds);
+        validTiles = new bool[tileArray.Length];
+
+        for (int i = 0; i < tileArray.Length; i++) {
+            if (tileArray[i] == normalTile) {
+                validTiles[i] = true;
+            }
+        }
+
+        towerScript = tower.GetComponent<TowerMovement>();
+    }
+
+    private void Update() {
+        //ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
+        Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        newMousePos = tilemap.WorldToCell(worldPoint);
+    }
+}
---
+++ Assets/Scenes/TowerPlacementButtonTEst/Test Script for Button/ToggleBuild.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleBuild : MonoBehaviour
+{
+    public PlaceTower1Test buildTower1;
+    public bool toggle;
+
+    void Start()
+    {
+        buildTower1 = this.GetComponent<PlaceTower1Test>();
+        buildTower1.enabled = false;
+    }
+
+    public void toggleBuild()
+    {
+        Debug.Log("Tower 1 Purchased");
+        buildTower1.enabled = !buildTower1.enabled;
+    }
+}
