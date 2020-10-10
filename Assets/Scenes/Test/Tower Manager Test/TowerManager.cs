9e29690a0e44dd817e22056fa033f3da495395f5
Cody Thompson <cody5887@yahoo.com>
2020-10-10 16:33:44 -0700

    Comments

---
+++ Assets/Scenes/Test/Tower Manager Test/TowerManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerManager : MonoBehaviour
+{
+    // Data members
+
+    /*
+    TowerManager[] AllTowers()
+    {
+
+    }
+
+    TowerManager GetTower(int x, int y)
+    {
+
+    }
+
+    */
+    bool TileOccupied(int x, int y)
+    {
+
+    }
+
+    void CreateTower(int x, int y)
+    {
+
+    }
+
+    RemoveTower(int x, int y)
+    {
+
+    }
+
+
+}
