2acfd5e2c117043022968ac62b2c42bdc83f8b6a
Cody Thompson <cody5887@yahoo.com>
2020-10-10 16:34:10 -0700

    Comments

--- Assets/Scenes/Test/Tower Manager Test/TowerManager.cs
+++ Assets/Scenes/Test/Tower Manager Test/TowerManager.cs
@@ -5,22 +5,27 @@ using UnityEngine;
 public class TowerManager : MonoBehaviour
 {
     // Data members
+    Tower[,] towerTracker;                          // 2D array of Towers
 
-    /*
-    TowerManager[] AllTowers()
+
+    void Awake()
+    {
+        towerTracker = new Tower[15, 15];           // Setup trackerArray
+    }
+
+    Tower[] AllTowers()
     {
 
     }
 
-    TowerManager GetTower(int x, int y)
+    Tower GetTower(int x, int y)
     {
 
     }
 
-    */
     bool TileOccupied(int x, int y)
     {
-
+        return towerTracker[x, y].active;           // Is the tower at (x, y) active?
     }
 
     void CreateTower(int x, int y)
@@ -28,10 +33,14 @@ public class TowerManager : MonoBehaviour
 
     }
 
-    RemoveTower(int x, int y)
+    void RemoveTower(int x, int y)
     {
 
     }
 
-
+    public class Tower
+    {
+        public string type;                         // The type of tower, if any
+        public bool active;                         // Is a tower active on this tile?
+    }
 }
