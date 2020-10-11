dc928e4f13920b55687ad250f170453e112c2046
Cody Thompson <cody5887@yahoo.com>
2020-10-10 17:42:19 -0700

    Not in working order, Need UI ouput for x and y

--- Assets/Scenes/Test/Tower Manager Test/TowerManager.cs
+++ Assets/Scenes/Test/Tower Manager Test/TowerManager.cs
@@ -1,46 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class TowerManager : MonoBehaviour
 {
     // Data members
     Tower[,] towerTracker;                          // 2D array of Towers
-
+    public GameObject tower;                        // Only one tower for testing purposes
+    Tower[] towerList;                              // List of towers
 
     void Awake()
     {
         towerTracker = new Tower[15, 15];           // Setup trackerArray
+        towerList = new Tower[255];
     }
 
     Tower[] AllTowers()
     {
-
+        return towerList;
     }
 
-    Tower GetTower(int x, int y)
+    GameObject GetTower(int x, int y)
     {
-
+        return towerTracker[x, y].type;
     }
 
     bool TileOccupied(int x, int y)
     {
-        return towerTracker[x, y].active;           // Is the tower at (x, y) active?
+        return !towerTracker[x, y].available;           // Is the tower at (x, y) active?
     }
 
-    void CreateTower(int x, int y)
+    public void CreateTower(int x, int y)                  
     {
+        if ( !TileOccupied(x, y) )
+        {
+            Instantiate(tower, new Vector3Int(x, y, 0), Quaternion.identity);   // Create tower object
+            towerTracker[x, y].available = false;                               // Tile is now unavailable
+            towerTracker[x, y].type = tower;                                    // Set tower reference
 
+        } 
+        else
+        {
+            UnityEngine.Debug.Log("Tile Occupied");         // Tile is already occupied
+        }
     }
 
     void RemoveTower(int x, int y)
     {
-
+        if ( TileOccupied(x, y) )
+        {
+            tower = GetTower(x, y);                          // Get tower reference
+            Destroy(tower);                                 // Destroy tower
+            towerTracker[x, y].available = true;            // Tile is now available
+        }
+        else
+        {
+            UnityEngine.Debug.Log("Nothing to destroy");    // Nothing was on the tile
+        }
     }
 
     public class Tower
     {
-        public string type;                         // The type of tower, if any
-        public bool active;                         // Is a tower active on this tile?
+        public GameObject type = null;                 // The type of tower, if any
+        public bool available = true;                  // Is the tile available?
     }
 }
