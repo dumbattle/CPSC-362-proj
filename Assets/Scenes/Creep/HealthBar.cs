0075f6737fa5448c1ddd42baedf0d35e4ed078de
Alejandro <harri171@csu.fullerton.edu>
2020-10-12 12:34:19 -0700

    Added Health bar

---
+++ Assets/Scenes/Creep/HealthBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+
+     private Transform bar;
+    // Start is called before the first frame update
+    void Start()
+    {
+          Transform bar = transform.find("Bar");
+    }
+
+     public void SetSize(float sizeNormal)
+     {
+
+          bar.localScale = new Vector3(sizeNormal, 1f);
+     }
+}
