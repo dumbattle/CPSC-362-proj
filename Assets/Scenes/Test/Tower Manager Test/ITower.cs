f0ad861563b03b63da3656a016ef264f5f06a3c9
Cody Thompson <cody5887@yahoo.com>
2020-10-18 16:41:24 -0700

    TowerManager redesign with ITower and exceptions

---
+++ Assets/Scenes/Test/Tower Manager Test/ITower.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public interface ITower
+{
+    Vector2Int mapIndex { get; }
+    void GameplayUpdate();
+}
--- Assets/Scenes/Test/Tower Manager Test/TowerManager.cs
+++ Assets/Scenes/Test/Tower Manager Test/TowerManager.cs
@@ -1,10 +1,29 @@
+<<<<<<< Updated upstream
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+=======
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+>>>>>>> Stashed changes
 using UnityEngine;
+using System;
 
-public class TowerManager : MonoBehaviour
+
+
+
+
+public class ActionFailedException : Exception
 {
+    public ActionFailedException(string msg) : base(msg) { }
+}
+
+
+
+public class TowerManager : MonoBehaviour, ITower
+{
+<<<<<<< Updated upstream
     // Data members
     Tower[,] towerTracker;                          // 2D array of Towers
     public GameObject tower;                        // Only one tower for testing purposes
@@ -17,10 +36,33 @@ public class TowerManager : MonoBehaviour
     }
 
     Tower[] AllTowers()
+=======
+    ITower[,] _towers;
+    private Vector2Int index;
+    public Vector2Int mapIndex { get { return index; } }
+    private int numOfTowers = 0;
+
+
+    void Awake()
     {
-        return towerList;
+        _towers = new ITower[15, 15];                                           // Setup trackerArray
+
+        for (int i = 0; i < 15; i++)
+        {
+            for (int j = 0; j < 15; j++)
+            {
+                _towers[i, j] = null;                                           // Initialize towerTracker
+            }
+        }
     }
 
+    public bool TileOccupied(int x, int y)
+>>>>>>> Stashed changes
+    {
+        return _towers[x, y] != null;
+    }
+
+<<<<<<< Updated upstream
     GameObject GetTower(int x, int y)
     {
         return towerTracker[x, y].type;
@@ -53,9 +95,58 @@ public class TowerManager : MonoBehaviour
             tower = GetTower(x, y);                          // Get tower reference
             Destroy(tower);                                 // Destroy tower
             towerTracker[x, y].available = true;            // Tile is now available
+=======
+    public ITower CreateTower(GameObject src, int x, int y)
+    {
+        ITower tt = src;
+        GameObject clone = null;
+        if (!TileOccupied(x, y))
+        {
+            index = new Vector2Int(x, y);
+            clone = Instantiate(src, new Vector3Int(x, y, 0), Quaternion.identity);                           // Create tower object
+            clone.transform.name = transform.name.Replace("TowerManager", "Tower1." + ++numOfTowers).Trim();    // Rename tower to Tower1.numOfTower
+            _towers[x, y] = tt;                                                                                 // Add src to _towers
+            return tt;
         }
         else
         {
+            throw new ActionFailedException("TowerManager::PlaceTower - Tile occupied at (" + x + ", " + y + ")");   // Tile is already occupied
+        }
+    }
+
+    public ITower GetTower(int x, int y)
+    {
+        if (TileOccupied(x, y))
+        {
+            return _towers[x, y];
+        }
+        else
+        {
+            throw new ActionFailedException("TowerManager::GetTower - Tile unoccupied at (" + x + ", " + y + ")");
+        }
+    }
+
+     public IEnumerable<ITower> AllTowers()
+     {
+        for (int x = 0; x < _towers.GetLength(0); x++)
+        {
+            for (int y = 0; y < _towers.GetLength(1); y++)
+            {
+                var t = _towers[x, y];
+                if (t != null)
+                {
+                    yield return t;
+                }
+            }
+>>>>>>> Stashed changes
+        }
+    }
+
+    public void GameplayUpdate()
+    {
+        foreach (var t in AllTowers())
+        {
+<<<<<<< Updated upstream
             UnityEngine.Debug.Log("Nothing to destroy");    // Nothing was on the tile
         }
     }
@@ -64,5 +155,24 @@ public class TowerManager : MonoBehaviour
     {
         public GameObject type = null;                 // The type of tower, if any
         public bool available = true;                  // Is the tile available?
+=======
+            t.GameplayUpdate();
+        }
+    }
+
+    public void RemoveTower(int x, int y)
+    {
+        GameObject towerToDestroy;
+        if (TileOccupied(x, y))
+        {
+            towerToDestroy = GetTower(x, y);                                        // Get tower reference
+            Destroy(towerToDestroy);                                                // Destroy tower
+            _towers[x, y] = null;
+        }
+        else
+        {
+            throw new ActionFailedException("TowerManager::RemoveTower - Tile unoccupied at (" + x + ", " + y + ")"); // Nothing was on the tile
+        }
+>>>>>>> Stashed changes
     }
 }
