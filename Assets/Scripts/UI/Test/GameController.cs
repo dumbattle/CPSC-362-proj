0498fd17049de8d4b9d860fe19b47a9f3e339dea
calvin ngo <calvin_ngo@csu.fullerton.edu>
2020-10-21 16:24:29 -0700

    combined ui manager

---
+++ Assets/Scripts/UI/Test/GameController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    delegate GameState GameState();
+
+    GameState gameState;
+
+    public CreepBehaviour cb;
+
+    void Start()
+    {
+        cb.Init();
+    }
+
+    private void Awake()
+    {
+        gameState = SceneStartState;
+    }
+
+    private void Update()
+    {
+        gameState = gameState() ?? gameState;
+        TestUIManager._main.CustomUpdate(); // check TestUIManager Line 78
+    }
+
+
+    GameState SceneStartState()
+    {
+        TestUIManager.SetPlayState();
+        return PlayState; // next state
+    }
+
+    GameState PlayState()
+    {
+        cb.GameplayUpdate();
+        if (TestUIManager.pausedReceived)
+        {
+            //Debug.Log("error");
+            TestUIManager.SetPauseState();
+            return PausedState;
+        }
+        return null;
+    }
+
+    GameState PausedState()
+    {
+        if (TestUIManager.playReceived)
+        {
+            TestUIManager.SetPlayState();
+            return PlayState;
+        }
+        return null;
+    }
+}
