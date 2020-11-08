96ba66da486113094d0dc8d27eaec8dee6606fb1
Cody Thompson <cody5887@yahoo.com>
2020-11-07 21:35:13 -0800

    Added creep sprites to TowerDev scene

--- Assets/Scripts/Tower/TowerBehaviour.cs
+++ Assets/Scripts/Tower/TowerBehaviour.cs
@@ -12,6 +12,14 @@ public abstract class TowerBehaviour : MonoBehaviour, ITower
     public abstract void GameplayUpdate();
     public abstract void WaitUpdate();
     public virtual void Init(Vector2Int index, TowerBehaviour src) { mapIndex = index; this.src = src; }
+
+    // inline or call this whenever a creep is targeted
+    protected void FaceTarget(CreepBehaviour cb)
+    {
+        var dir = cb.transform.position - transform.position;
+        // set forward axis to point to creep
+        transform.up = dir;
+    }
 }
 
 // To add functionality, it must be done in both ITower and TowerBehaviour
