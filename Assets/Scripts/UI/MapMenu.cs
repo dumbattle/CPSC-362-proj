e6dd55afbe89b19bac092fc192bbb43e1f9a6012
justinkhado <justinkhado@gmail.com>
2020-11-08 18:43:33 -0800

    added 2 new maps and map menu

---
+++ Assets/Scripts/UI/MapMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapMenu : MonoBehaviour
+{
+    public void PlayGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void SelectMap1() {
+        SceneManager.LoadScene(3);
+    }
+
+    public void SelectMap2() {
+        SceneManager.LoadScene(4);
+    }
+
+    public void SelectMap3() {
+        SceneManager.LoadScene(5);
+    }
+}
